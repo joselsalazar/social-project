9f11528b988d87f2efda6e2691e93e760bb762a7
sal48 <sal.salazar1@gmail.com>
2017-11-12 12:02:38 -0700

    v .5
    basic implementation of places api, using geolocation (prob still need to have nice defaults for inputting manual addresses, or refusing to share location)

--- public/js/google-places.js
+++ public/js/google-places.js
@@ -22,6 +22,42 @@ function initMap() {
   }, callback);
 }
 
+function launchPlacesUsingGeolocation() {
+  // Note: This requires that you consent to location sharing when
+  // prompted by your browser. If you see the error "The Geolocation service
+  // failed.", it means you probably did not give permission for the browser to
+  // locate you (default Phoenix Arizona coordinates)
+  var map, infoWindow;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.4484, lng: 112.0740},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  $( "#map" ).toggle(true);
+  $( "#intro-header" ).toggle(false);
+}
+
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
